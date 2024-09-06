51d81ca4e003e85116711ab11a68991a2e767d1d
Selim Boulaaba <selimboulaaba00@gmail.com>
2024-09-06 19:51:53 +0100

    fix migration

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 const db = async () =>{
     try {
         await mongoose.connect(
-            'mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@personalapps.8f3hh.mongodb.net/?retryWrites=true&w=majority&appName=PersonalApps'
+            'mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@personalapps.8f3hh.mongodb.net/connect_4?retryWrites=true&w=majority&appName=PersonalApps'
           );
         console.log("Connected to MongoDB")
     } catch (error) {
