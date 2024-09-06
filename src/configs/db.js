ff26726c1d877aa9e599ae28c1f2c280e3eb01b8
Selim Boulaaba <selimboulaaba00@gmail.com>
2024-09-06 19:17:52 +0100

    database migration

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 const db = async () =>{
     try {
         await mongoose.connect(
-            'mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@connect-4.8f3hh.mongodb.net/App?retryWrites=true&w=majority&appName=Connect-4'
+            'mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@personalapps.8f3hh.mongodb.net/?retryWrites=true&w=majority&appName=PersonalApps'
           );
         console.log("Connected to MongoDB")
     } catch (error) {
