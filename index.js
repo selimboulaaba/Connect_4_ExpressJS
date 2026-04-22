c41902ba29850a32d5b2d267b9a8a5fc3fe26fb8
selimboulaaba <selimboulaaba00@gmail.com>
2026-04-22 23:33:29 +0100

    add dockerfile

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require("./src/configs/db")
 const cookieParser = require("cookie-parser")
 const socketSetup = require('./src/sockets/socket');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 const app = express()
 app.use(express.json())
 app.use(cors())
